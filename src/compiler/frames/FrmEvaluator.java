8a71689f2ae5bfb37f16616cdf2895698801934e
Klemen Košir <klemen.kosir@kream.io>
2015-04-11 21:06:08 +0200

    frames: Added Visitor functions.

--- src/compiler/frames/FrmEvaluator.java
+++ src/compiler/frames/FrmEvaluator.java
@@ -1,9 +1,99 @@
 package compiler.frames;
 
 import compiler.abstr.*;
+import compiler.abstr.tree.*;
 
-public class FrmEvaluator implements Visitor {
-	
-	// TODO
-	
+public class FrmEvaluator implements Visitor
+{
+	public void visit(AbsArrType acceptor)
+	{
+	}
+
+	public void visit(AbsAtomConst acceptor)
+	{
+	}
+
+	public void visit(AbsAtomType acceptor)
+	{
+	}
+
+	public void visit(AbsBinExpr acceptor)
+	{
+	}
+
+	public void visit(AbsComp acceptor)
+	{
+	}
+
+	public void visit(AbsCompName acceptor)
+	{
+	}
+
+	public void visit(AbsDefs acceptor)
+	{
+	}
+
+	public void visit(AbsExprs acceptor)
+	{
+	}
+
+	public void visit(AbsFor acceptor)
+	{
+	}
+
+	public void visit(AbsFunCall acceptor)
+	{
+	}
+
+	public void visit(AbsFunDef acceptor)
+	{
+	}
+
+	public void visit(AbsIfThen acceptor)
+	{
+	}
+
+	public void visit(AbsIfThenElse acceptor)
+	{
+	}
+
+	public void visit(AbsPar acceptor)
+	{
+	}
+
+	public void visit(AbsPtrType acceptor)
+	{
+	}
+
+	public void visit(AbsRecType acceptor)
+	{
+	}
+
+	public void visit(AbsTypeDef acceptor)
+	{
+	}
+
+	public void visit(AbsTypeName acceptor)
+	{
+	}
+
+	public void visit(AbsUnExpr acceptor)
+	{
+	}
+
+	public void visit(AbsVarDef acceptor)
+	{
+	}
+
+	public void visit(AbsVarName acceptor)
+	{
+	}
+
+	public void visit(AbsWhere acceptor)
+	{
+	}
+
+	public void visit(AbsWhile acceptor)
+	{
+	}
 }
