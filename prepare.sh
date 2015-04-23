8a9a227405697e30c74943a395701351633be902
Klemen Košir <klemen.kosir@kream.io>
2015-04-23 21:33:42 +0200

    Updated prepare script.

--- prepare.sh
+++ prepare.sh
@@ -4,6 +4,10 @@ mkdir -p release/prev
 cp -r src/ release/prev/src/
 find release/prev/ -name "*.class" -type f -delete
 cd release
-rm 63120217-$1.zip
+
+if [ -f 63120217-$1.zip ]; then
+	rm 63120217-$1.zip
+fi
+
 zip -r 63120217-$1.zip prev/
 rm -r prev/
