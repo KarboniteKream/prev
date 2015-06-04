d2a3dfb40db5728164e75c5c8b44c633d139a959
Klemen Košir <klemen.kosir@kream.io>
2015-06-05 00:58:00 +0200

    regalloc: Implemented build(), simplify(), spill() and select().

--- src/compiler/regalloc/RegAlloc.java
+++ src/compiler/regalloc/RegAlloc.java
@@ -11,37 +11,152 @@ import compiler.tmpan.*;
 public class RegAlloc
 {
 	private boolean dump;
-	private int R;
+	private int registers;
 
-	public RegAlloc(boolean dump, int R)
+	private TmpAn tmpan;
+	private LinkedList<TmpNode> stack;
+
+	public RegAlloc(boolean dump, int registers)
 	{
 		this.dump = dump;
-		this.R = R;
+		this.registers = registers;
+
+		tmpan = new TmpAn(false);
+		stack = null;
 	}
 
 	public void allocate(LinkedList<ImcChunk> chunks)
 	{
+		for(ImcChunk chunk : chunks)
+		{
+			if(chunk instanceof ImcCodeChunk == true)
+			{
+				ImcCodeChunk codeChunk = (ImcCodeChunk)chunk;
+
+				do
+				{
+					build(codeChunk);
+					stack = new LinkedList<TmpNode>();
+
+					do
+					{
+						simplify(codeChunk);
+					} while(spill(codeChunk) == true);
+				} while(select(codeChunk) == true);
+
+				codeChunk.registers = new HashMap<FrmTemp, String>();
+				codeChunk.registers.put(codeChunk.frame.FP, "$251");
+
+				for(TmpNode node : codeChunk.graph)
+				{
+					codeChunk.registers.put(node.temp, "$" + node.register);
+				}
+			}
+		}
 	}
 
 	private void build(ImcCodeChunk chunk)
 	{
+		tmpan.analyze(chunk);
 	}
 
 	private void simplify(ImcCodeChunk chunk)
 	{
+		boolean done = false;
+
+		while(done == false)
+		{
+			done = true;
+
+			Iterator<TmpNode> iterator = chunk.graph.iterator();
+
+			while(iterator.hasNext() == true)
+			{
+				TmpNode node = iterator.next();
+
+				if(node.edges.size() < registers)
+				{
+					done = false;
+
+					stack.add(node);
+
+					for(TmpNode edge : chunk.graph)
+					{
+						edge.edges.remove(node);
+					}
+
+					iterator.remove();
+				}
+			}
+		}
 	}
 
-	private void spill()
+	private boolean spill(ImcCodeChunk chunk)
 	{
+		if(chunk.graph.size() == 0)
+		{
+			return false;
+		}
+
+		TmpNode node = chunk.graph.removeFirst();
+		System.out.println(node.temp.name());
+		node.spill = TmpNode.POTENTIAL_SPILL;
+		stack.add(node);
+
+		for(TmpNode edge : node.edges)
+		{
+			edge.edges.remove(node);
+		}
+
+		return true;
 	}
 
-	private void select()
+	private boolean select(ImcCodeChunk chunk)
 	{
+		boolean repeat = false;
+		Iterator<TmpNode> iterator = stack.descendingIterator();
+
+		while(iterator.hasNext() == true)
+		{
+			TmpNode node = iterator.next();
+			chunk.graph.add(node);
+
+			int regs[] = new int[registers];
+
+			for(TmpNode edge : node.edges)
+			{
+				regs[edge.register] = 1;
+			}
+
+			boolean ok = false;
+			for(int i = 0; i < registers; i++)
+			{
+				if(regs[i] == 0)
+				{
+					node.register = i;
+					ok = true;
+					break;
+				}
+			}
+
+			if(ok == false)
+			{
+				node.spill = TmpNode.ACTUAL_SPILL;
+				repeat = true;
+				break;
+			}
+		}
+
+		if(repeat == true)
+		{
+			startOver();
+		}
+
+		return repeat;
 	}
 
 	private void startOver()
 	{
-
 	}
 
 	public void dump(LinkedList<ImcChunk> chunks)
--- src/compiler/tmpan/TmpNode.java
+++ src/compiler/tmpan/TmpNode.java
@@ -6,18 +6,21 @@ import compiler.frames.*;
 
 public class TmpNode
 {
+	public static final int POTENTIAL_SPILL = 1;
+	public static final int ACTUAL_SPILL = 2;
+
 	public FrmTemp temp;
 	public LinkedList<TmpNode> edges;
 
 	public int register;
-	public boolean spill;
+	public int spill;
 
 	public TmpNode(FrmTemp temp)
 	{
 		this.temp = temp;
 		edges = new LinkedList<TmpNode>();
 
-		register = null;
-		spill = false;
+		register = 0;
+		spill = 0;
 	}
 }
