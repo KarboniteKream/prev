5cb0e7a3d1bebf439d6a90c32a616cd672a8ee36
Klemen Košir <klemen.kosir@kream.io>
2015-06-04 23:01:48 +0200

    Updated compilation script.

--- compile.sh
+++ compile.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-javac -cp src src/compiler/Main.java && java -cp src compiler.Main --phase=$1 --dump=$1 test/$2.prev && cat test/$2.log
+
+REGISTERS=${3:-8}
+
+javac -cp src src/compiler/Main.java && java -cp src compiler.Main --phase=$1 --dump=$1 --registers=$REGISTERS test/$2.prev && cat test/$2.log
