58ac2c71b88bee5ce8af23a4cc3dffc919d14d95
Klemen Košir <klemen.kosir@kream.io>
2015-06-11 23:21:02 +0200

    Updated helper scripts.

--- clean.sh
+++ clean.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 find src/ -name "*.class" -type f -delete
-find tests/ -name "*.log" -type f -delete
+find tests/ \( -name "*.log" -o -name "*.mms" -o -name "*.mmo" \) -type f -delete
---
+++ run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+mmixal tests/$1.mms
+mmix tests/$1.mmo
